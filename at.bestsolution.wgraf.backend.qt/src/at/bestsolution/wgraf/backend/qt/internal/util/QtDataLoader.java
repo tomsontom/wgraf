c2b83f3666f7a5c05de0759b939ae6a0f844514e
Tom Schindl <tom.schindl@bestsolution.at>
2014-02-13 14:29:28 +0100

    added support for data URIs

--- at.bestsolution.wgraf.backend.qt/src/at/bestsolution/wgraf/backend/qt/internal/util/QtDataLoader.java
+++ at.bestsolution.wgraf.backend.qt/src/at/bestsolution/wgraf/backend/qt/internal/util/QtDataLoader.java
@@ -5,33 +5,37 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+
+import org.apache.commons.codec.binary.Base64;
 
 import at.bestsolution.wgraf.style.FontSource;
 
 import com.trolltech.qt.core.QByteArray;
-import com.trolltech.qt.gui.QFont;
 import com.trolltech.qt.gui.QFontDatabase;
 
 public class QtDataLoader {
 
-	
+
 	// TODO cache the stuff
 	public static byte[] loadURI(URI uri) throws IOException {
-		InputStream in = uri.toURL().openStream();
-		ByteArrayOutputStream o = new ByteArrayOutputStream();
-		int nRead;
-		byte[] data = new byte[1024];
-		while ((nRead = in.read(data, 0, data.length)) != -1) {
-		  o.write(data, 0, nRead);
+		if( "data".equals(uri.getScheme()) ) {
+			return Base64.decodeBase64(uri.getSchemeSpecificPart().getBytes());
+		} else {
+			InputStream in = uri.toURL().openStream();
+			ByteArrayOutputStream o = new ByteArrayOutputStream();
+			int nRead;
+			byte[] data = new byte[1024];
+			while ((nRead = in.read(data, 0, data.length)) != -1) {
+			  o.write(data, 0, nRead);
+			}
+			in.close();
+			return o.toByteArray();
 		}
-		in.close();
-		return o.toByteArray();
 	}
-	
+
 	private static Map<URI, Integer> loadedFonts = new HashMap<URI, Integer>();
-	
+
 	public static int loadFont(FontSource source) {
 		if (loadedFonts.containsKey(source.source)) {
 			return loadedFonts.get(source.source);
